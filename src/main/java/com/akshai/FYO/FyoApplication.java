ec3d6c4b191721ee20c8c15625f23714befa91ba
Akshai-Athota <akshaiathotaspring001@gmail.com>
2024-07-26 19:45:51 +0530

    intial folder and git set up

---
+++ src/main/java/com/akshai/FYO/FyoApplication.java
@@ -0,0 +1,13 @@
+package com.akshai.FYO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FyoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FyoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akshai/FYO/FyoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshai.FYO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FyoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
